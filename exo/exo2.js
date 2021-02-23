5cbbc65d89d08ed0cef88f0fe9a684739eb73d2a
Dan <dan.esposito@ynov.com>
2021-02-23 15:43:54 +0100

    exo 2

--- exo/exo2.js
+++ exo/exo2.js
@@ -1,3 +1,8 @@
 function add(number) {
-
+    number.toString().split(",");
+    let result = 0;
+    number.split(",").forEach(
+        element => result += parseFloat(element)
+    );
+    return result
 }
